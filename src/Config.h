3397def10dd8b65f0be2638d7c13c18ec05df477
asirindusamod99 <asirindusamod99@email.com>
2026-07-30 10:02:02 +0530

    Full updated

--- src/Config.h
+++ src/Config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // Firmware Version definition for Web UI / Firebase tracking
-#define FIRMWARE_VERSION "1.05"
+#define FIRMWARE_VERSION "1.056"
 
 // Wi-Fi credentials.
 // ⚠️ TODO: rotate this password and move it out of source control
